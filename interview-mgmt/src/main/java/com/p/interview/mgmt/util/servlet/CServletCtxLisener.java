18a88a842deb35825e74e7d1912f7f74a1fdfbe9
Premendra Kumar <premendra.bce05515@gmail.com>
2017-10-20 08:39:03 +0530

    committing initial code to create backup for category with questions and their answers

--- interview-mgmt/src/main/java/com/p/interview/mgmt/util/servlet/CServletCtxLisener.java
+++ interview-mgmt/src/main/java/com/p/interview/mgmt/util/servlet/CServletCtxLisener.java
@@ -9,7 +9,7 @@ import org.apache.log4j.Logger;
 
 public class CServletCtxLisener implements ServletContextListener {
 
-	private static String confFolderPath="F:/APPN_SERVERS/apache-tomcat-8.5.15/apache-tomcat-8.5.15/webapps/interview-mgmt/WEB-INF/conf";
+	private static String confFolderPath="E:/APPSERVERS/CNX-PROD-apache-tomcat-8.0.35/webapps/interview-mgmt/WEB-INF/conf";
 	private static String contextPath;
 	static Logger log = Logger.getLogger(CServletCtxLisener.class);
 
