032b4c754564755884a421921ae435fbcef34af3
Premendra.Kumar <premendra.bce05515@gmail.com>
2019-06-08 15:07:54 +0530

    commit

--- interview-mgmt/src/main/java/com/p/interview/mgmt/resources/ReportResource.java
+++ interview-mgmt/src/main/java/com/p/interview/mgmt/resources/ReportResource.java
@@ -6,23 +6,16 @@ import java.util.List;
 import java.util.Vector;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
 import org.apache.log4j.Logger;
 
-import com.p.interview.mgmt.exception.RestServiceException;
 import com.p.interview.mgmt.pojo.CategoryDTO;
 import com.p.interview.mgmt.pojo.QuestionDTO;
 import com.p.interview.mgmt.pojo.QuestionJsonResponseForReport;
@@ -75,6 +68,59 @@ public class ReportResource {
 		}
 
 		return Response.status(HttpURLConnection.HTTP_OK).entity(listQuestionJsonResponseForReport).build();
+	}
+	
+	
+
+	/**
+	 * Gets the all categories list.
+	 *
+	 * @return the all categories list
+	 */
+	@GET
+	@Path("/allQuestions/categories/{categoryId}/questions/{questionId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAllCategories(@Context HttpServletRequest serveletRequest,@PathParam("categoryId") int categoryId, @PathParam("questionId") int questionId) {
+
+		logger.info(
+				"Entered into com.p.interview.mgmt.resources.CategoryResource.getAllCategories(HttpServletRequest) method");
+		String message = "successfully contacted the restful API server";
+		logger.info("Information : " + message);
+		logger.info(
+				"com.p.interview.mgmt.resources.CategoryResource.getAllCategories(HttpServletRequest) method called");
+
+//		Vector<CategoryDTO> list = new Vector<>();
+//		List<QuestionJsonResponseForReport> listQuestionJsonResponseForReport = new ArrayList<QuestionJsonResponseForReport>();
+		QuestionJsonResponseForReport qr;
+		try {
+			CategoryDTO catdto=new CategoryDTO();
+			catdto.setCatID(categoryId);
+			catdto = objInterviewRPC.retrieveCategory(catdto);
+			
+			QuestionDTO qdto=new QuestionDTO();
+			qdto.setQuestionID(questionId);
+			qdto.setLinkedCatID(categoryId);
+			
+			qdto=objInterviewRPC.retrieveQuestion(qdto);
+			
+			qr=new QuestionJsonResponseForReport(catdto, qdto);
+			
+//			list = objInterviewRPC.fetchAllCategories();
+//			if (list != null) {
+//				for (CategoryDTO c : list) {
+//					if (c.getQuestions() != null) {
+//						for (QuestionDTO q : c.getQuestions()) {
+//							listQuestionJsonResponseForReport.add(new QuestionJsonResponseForReport(c, q));
+//						}
+//					}
+//				}
+//			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(HttpURLConnection.HTTP_NOT_FOUND).entity(e).build();
+		}
+
+		return Response.status(HttpURLConnection.HTTP_OK).entity(qr).build();
 	}
 
 }
